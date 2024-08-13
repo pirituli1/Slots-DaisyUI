f907e0d06cb694064063d82b758d53475c2c466c
Bautista Velasco Carlos Enrique <150282401+pirituli1@users.noreply.github.com>
2024-08-13 14:40:41 -0600

    Se termina la sección 11

--- src/modules/common/components/InputModal.vue
+++ src/modules/common/components/InputModal.vue
@@ -1,21 +1,21 @@
 <template>
   <dialog id="my_modal_1" class="modal" :open="open">
     <div class="modal-box">
-      <h3 class="text-lg font-bold">Hello!</h3>
-      <p class="py-4">Press ESC key or click the button below to close</p>
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p v-if="subtitle" class="py-4">{{ subtitle }}</p>
 
       <div class="modal-action flex flex-col">
         <form method="dialog" @submit.prevent="submitValue">
           <input
             ref="inputRef"
             type="text"
-            placeholder="Nombre del royecto"
+            :placeholder="placeholder ?? 'Ingrese un valor'"
             class="input input-bordered input-primary w-full flex-1"
             v-model="inputValue"
           />
           <!-- if there is a button in form, it will close the modal -->
           <div class="flex justify-end mt-5">
-            <button class="btn mr-4">Close</button>
+            <button class="btn mr-4" @click="emits('close')">Close</button>
             <button type="submit" class="btn btn-primary">Aceptar</button>
           </div>
         </form>
@@ -33,6 +33,9 @@
 import { ref } from 'vue';
 interface Props {
   open: boolean;
+  title: string;
+  subtitle?: string;
+  placeholder?: string;
 }
 defineProps<Props>();
 
@@ -45,7 +48,6 @@ const inputValue = ref('');
 const inputRef = ref<HTMLInputElement | null>(null);
 
 const submitValue = () => {
-  console.log({ value: inputValue.value });
   if (!inputValue.value) {
     // Foco en el elemento
     inputRef.value?.focus();
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -22,19 +22,59 @@
     </table>
   </div>
 
-  <input-modal :open="modalOpen" />
+  <!-- <input-modal :open="modalOpen" @close="modalOpen = false" @value="onNewValue($event)" /> Si se utiliza solo el argumento es inecesario mandarle el event -->
+  <input-modal
+    :open="modalOpen"
+    @close="modalOpen = false"
+    @value="onNewValue"
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo Proyecto"
+    subtitle="Dale un nombre único a tu proyecto"
+  />
+
+  <custom-modal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Titulo del modal</h1>
+    </template>
+
+    <template #body>
+      <p>cuerpo de mi custom modal</p>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-end">
+        <div class="flex justify-end mt-5">
+          <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+          <button class="btn btn-primary">Aceptar</button>
+        </div>
+      </div>
+    </template>
+  </custom-modal>
 
   <fab-button @click="modalOpen = true">
     <AddCircle />
   </fab-button>
+
+  <fab-button @click="customModalOpen = true" position="bottom-left">
+    <Modallcon />
+  </fab-button>
 </template>
 
 <script lang="ts" setup>
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 import FabButton from '@/modules/common/components/FabButton.vue';
 import InputModal from '@/modules/common/components/InputModal.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import Modallcon from '@/modules/common/icons/Modallcon.vue';
 
 import { ref } from 'vue';
 
 const modalOpen = ref(false);
+const customModalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log({
+    projectName: projectName,
+  });
+};
 </script>
